79e55947e83d40c7399644f60531c817b6baf8ed
AltAdamNguyen <ngotuananh2101@gmail.com>
2022-04-18 16:11:56 +0700

    release layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { WeatherComponent } from './weather/weather.component';
+import { MapComponent } from './map/map.component';
+import { StaticComponent } from './static/static.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ChartComponent } from './chart/chart.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    WeatherComponent,
+    MapComponent,
+    StaticComponent,
+    FooterComponent,
+    AboutComponent,
+    ChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/brief.model.ts
@@ -0,0 +1,9 @@
+export interface Brief {
+    confirmed: number;
+    deaths: number;
+    recovered: number;
+    active: number;
+    lastUpdate: string;
+    country: string;
+
+}
